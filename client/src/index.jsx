9e71df449674044e2a27c3c1eecff54cbd10cd3a
nathan-claeys <127297857+nathan-claeys@users.noreply.github.com>
2025-01-10 17:21:58 +0100

    feat: enhance file upload validation for user profile

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -7,20 +7,30 @@ import { NotFound } from './pages/_404.jsx';
 import './style.css';
 import { useState } from 'preact/hooks';
 import defaultProfile from './utils/defaultProfile.json';
+import { checkIsProfile } from './helpers/profileCheck.js';
 
 export function App() {
 	const [logedIn, setLogedIn] = useState(false);
 	const [profile, setProfile] = useState(defaultProfile);
 
 	const handleFileUpload = (file) => {
-		//read json file and set profile
+		const uploadedFile = file.target.files[0];
+		if (uploadedFile.type !== 'application/json') {
+			alert('Invalid file type. Please upload a JSON file.');
+			return;
+		}
+
 		const reader = new FileReader();
 		reader.onload = (e) => {
 			const profile = JSON.parse(e.target.result.toString());
+			if (!checkIsProfile(profile)) {
+				alert('Invalid profile');
+				return;			
+			}
 			setProfile(profile);
+			setLogedIn(true);
 		}
-		reader.readAsText(file.target.files[0]);
-		setLogedIn(true);
+		reader.readAsText(uploadedFile);
 	  };
 
 	return (
