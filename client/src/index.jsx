e6bbe89393608bbb640b9014f7e56796c998a2e8
nathan-claeys <127297857+nathan-claeys@users.noreply.github.com>
2025-01-10 17:05:35 +0100

    Implement user profile upload and conditional rendering in App component

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -10,13 +10,17 @@ import MusicManager from './pages/MusicManager/index.jsx';
 import './style.css';
 import { useState } from 'preact/hooks';
 import defaultProfile from './utils/defaultProfile.json';
+<<<<<<< HEAD
 import { checkIsProfile } from './helpers/profileCheck.js';
+=======
+>>>>>>> b61f924 (Implement user profile upload and conditional rendering in App component)
 
 export function App() {
 	const [logedIn, setLogedIn] = useState(false);
 	const [profile, setProfile] = useState(defaultProfile);
 
 	const handleFileUpload = (file) => {
+<<<<<<< HEAD
 		const uploadedFile = file.target.files[0];
 		if (uploadedFile.type !== 'application/json') {
 			alert('Invalid file type. Please upload a JSON file.');
@@ -34,6 +38,16 @@ export function App() {
 			setLogedIn(true);
 		}
 		reader.readAsText(uploadedFile);
+=======
+		//read json file and set profile
+		const reader = new FileReader();
+		reader.onload = (e) => {
+			const profile = JSON.parse(e.target.result.toString());
+			setProfile(profile);
+		}
+		reader.readAsText(file.target.files[0]);
+		setLogedIn(true);
+>>>>>>> b61f924 (Implement user profile upload and conditional rendering in App component)
 	  };
 
 	return (
@@ -49,7 +63,10 @@ export function App() {
 					<main>
 						<Router>
 							<Route path="/" component={() => <Home profile={profile} />} />
+<<<<<<< HEAD
 							<Route path='/music' component={() => <MusicManager />} />
+=======
+>>>>>>> b61f924 (Implement user profile upload and conditional rendering in App component)
 							<Route default component={NotFound} />
 						</Router>
 					</main>
