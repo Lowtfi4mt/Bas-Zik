94aa378e0b5e2e385d960ee2a4631f960244298f
Phoenixcresset <jeanbaptiste.lambertin@gmail.com>
2025-01-10 18:44:32 +0100

    Docker : added dev environment

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,11 @@ import preact from '@preact/preset-vite';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [preact()],
+	server: {
+		port: 5173,
+		host: '0.0.0.0',
+		watch: {
+			usePolling: true
+		}
+	}
 });
