d688be7ac819a139680c23f31b98aa835f38309e
nathan-claeys <127297857+nathan-claeys@users.noreply.github.com>
2025-01-10 17:05:35 +0100

    Implement user profile upload and conditional rendering in App component

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,22 +1,47 @@
-import { render } from 'preact';
+import { render, h } from 'preact';
 import { LocationProvider, Router, Route } from 'preact-iso';
 
 import { Header } from './components/Header.jsx';
 import { Home } from './pages/Home/index.jsx';
 import { NotFound } from './pages/_404.jsx';
 import './style.css';
+import { useState } from 'preact/hooks';
+import defaultProfile from './utils/defaultProfile.json';
 
 export function App() {
+	const [logedIn, setLogedIn] = useState(false);
+	const [profile, setProfile] = useState(defaultProfile);
+
+	const handleFileUpload = (file) => {
+		//read json file and set profile
+		const reader = new FileReader();
+		reader.onload = (e) => {
+			const profile = JSON.parse(e.target.result.toString());
+			setProfile(profile);
+		}
+		reader.readAsText(file.target.files[0]);
+		setLogedIn(true);
+	  };
+
 	return (
-		<LocationProvider>
-			<Header />
-			<main>
-				<Router>
-					<Route path="/" component={Home} />
-					<Route default component={NotFound} />
-				</Router>
-			</main>
-		</LocationProvider>
+		<div>
+			{!logedIn ? (
+				<div>
+					<button onClick={() => setLogedIn(true)}>Profil invité</button>
+					<input type="file" onChange={(e) => handleFileUpload(e)} />
+				</div>
+			) : (
+				<LocationProvider>
+					<Header />
+					<main>
+						<Router>
+							<Route path="/" component={() => <Home profile={profile} />} />
+							<Route default component={NotFound} />
+						</Router>
+					</main>
+				</LocationProvider>
+			)}
+		</div>
 	);
 }
 
