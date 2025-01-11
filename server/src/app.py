6b907566a54994073eae15ebc2a71555e657a45e
Phoenixcresset <jeanbaptiste.lambertin@gmail.com>
2025-01-11 15:32:37 +0100

    feat : Added example flask_smorest app

--- server/src/app.py
+++ server/src/app.py
@@ -1,19 +1,57 @@
-from flask import Flask, jsonify, request
+"""
+Simple Flask API
+"""
+
+from flask import Flask
+from flask.views import MethodView
+from flask_smorest import Api, Blueprint
+from marshmallow import Schema, fields
 
 app = Flask(__name__)
+app.config["API_TITLE"] = "My API"
+app.config["API_VERSION"] = "v1"
+app.config["OPENAPI_VERSION"] = "3.0.3"
+app.config["OPENAPI_URL_PREFIX"] = "/docs"
+app.config["OPENAPI_SWAGGER_UI_PATH"] = "/apidocs"
+app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
 
-@app.route('/')
-def home():
-    return jsonify({"message": "Welcome to Flask Backend!"})
+api = Api(app)
 
-@app.route('/api/data', methods=['GET'])
-def get_data():
-    return jsonify({"data": "Example Data"})
+blp = Blueprint("items", __name__, url_prefix="/items", description="Items operations")
 
-@app.route('/api/post', methods=['POST'])
-def post_data():
-    content = request.json
-    return jsonify({"received": content})
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+class ItemSchema(Schema):
+    """
+    This is a class docstring
+    """
+
+    id = fields.Int(
+        required=True,
+        description="The item ID",
+        example=1,
+    )
+    name = fields.Str(
+        required=True,
+        description="The item name",
+        example="My Item",
+    )
+
+
+@blp.route("/")
+class ItemList(MethodView):
+    """
+    This is a class docstring
+    """
+
+    @blp.response(200, ItemSchema(many=True))
+    def get(self):
+        """Get all items"""
+        print(app.url_map)
+        return [{"id": 1, "name": "Item One"}, {"id": 2, "name": "Item Two"}]
+
+
+api.register_blueprint(blp)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
