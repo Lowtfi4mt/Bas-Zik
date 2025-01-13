6742d8ef0d756b15fc1fdbab88fce5e2afed8fa7
Phoenixcresset <jeanbaptiste.lambertin@gmail.com>
2025-01-13 22:00:01 +0100

    refactor : created create_app function for better readability

--- server/src/app.py
+++ server/src/app.py
@@ -3,25 +3,28 @@ Simple Flask API
 """
 
 from flask import Flask
-from flask.views import MethodView
-from flask_smorest import Api, Blueprint
+from flask_smorest import Api
 
-app = Flask(__name__)
-app.config["API_TITLE"] = "My API"
-app.config["API_VERSION"] = "v1"
-app.config["OPENAPI_VERSION"] = "3.0.3"
-app.config["OPENAPI_URL_PREFIX"] = "/docs"
-app.config["OPENAPI_SWAGGER_UI_PATH"] = "/apidocs"
-app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
 
-api = Api(app)
-
+def create_app():
     """
+    Create the Flask app
     """
+    app = Flask(__name__)
+    app.config["API_TITLE"] = "My API"
+    app.config["API_VERSION"] = "v1"
+    app.config["OPENAPI_VERSION"] = "3.0.3"
+    app.config["OPENAPI_URL_PREFIX"] = "/docs"
+    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/apidocs"
+    app.config["OPENAPI_SWAGGER_UI_URL"] = (
+        "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
     )
 
+    api = Api(app)
 
+    return app
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    main_app = create_app()
+    main_app.run(host="0.0.0.0", port=5000, debug=True)
