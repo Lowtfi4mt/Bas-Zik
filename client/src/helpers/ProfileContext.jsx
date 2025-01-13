e494f8581d53414c6d00caae47c253d80b2ec7fc
nathan-claeys <127297857+nathan-claeys@users.noreply.github.com>
2025-01-13 12:01:26 +0100

    feat: add ProtectedRoute and ProfileContext for user authentication

---
+++ client/src/helpers/ProfileContext.jsx
@@ -0,0 +1,38 @@
+import { createContext } from 'preact';
+import { useContext, useEffect, useState } from 'preact/hooks';
+
+const ProfileContext = createContext(null);
+
+export const ProfileProvider = ({ children }) => {
+  const [profile, setProfile] = useState(() => {
+    const profileData = localStorage.getItem('profile');
+    return profileData ? JSON.parse(profileData) : null;
+  });
+
+  useEffect(() => {
+    if (profile) {
+      localStorage.setItem('profile', JSON.stringify(profile)); // Sérialiser l'objet
+    } else {
+      localStorage.removeItem('profile');
+    }
+  }, [profile]);
+
+  const login = (profileData) => {
+    setProfile(profileData);
+  };
+
+  const logout = () => {
+    setProfile(null);
+    localStorage.removeItem('profile');
+  };
+
+  return (
+    <ProfileContext.Provider value={{ profile, login, logout }}>
+      {children}
+    </ProfileContext.Provider>
+  );
+};
+
+export const useProfile = () => {
+  return useContext(ProfileContext);
+};
