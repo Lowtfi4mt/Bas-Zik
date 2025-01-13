54b6e42ac14529b8b9786050a2e29a19cafdd0d5
Phoenixcresset <jeanbaptiste.lambertin@gmail.com>
2025-01-13 22:58:53 +0100

    feat : added basic mock data function for testing purposes

--- server/src/app.py
+++ server/src/app.py
@@ -2,14 +2,19 @@
 Simple Flask API
 """
 
+import random
+from faker import Faker
 from flask import Flask
 from flask_smorest import Api
 from musics import musics_blp
 from authors import authors_blp
 from albums import albums_blp
+from models import db, Music, Author, Album
+
+USE_MOCKS = True
 
 
-def create_app():
+def create_app() -> Flask:
     """
     Create the Flask app
     """
@@ -22,13 +27,41 @@ def create_app():
     app.config["OPENAPI_SWAGGER_UI_URL"] = (
         "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
     )
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["SQLALCHEMY_ECHO"] = True
 
     api = Api(app)
     api.register_blueprint(musics_blp)
     api.register_blueprint(authors_blp)
     api.register_blueprint(albums_blp)
+    
+    db.init_app(app)
+    with app.app_context():
+        db.drop_all()
+        db.create_all()
+
+    if USE_MOCKS:
+        create_mock_data(app)
 
     return app
+
+
+def create_mock_data(app):
+    with app.app_context():
+        fake = Faker()
+
+        # Generate authors
+        for _ in range(10):
+            author = Author(name=fake.name())
+            db.session.add(author)
+
+        # Generate albums
+        for _ in range(10):
+            album = Album(name=fake.sentence())
+            db.session.add(album)
+
+        db.session.commit()
 
 
 if __name__ == "__main__":
