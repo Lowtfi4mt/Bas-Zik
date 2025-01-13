a68ed4f280b36291b6902bf086e77a447395371b
nathan-claeys <127297857+nathan-claeys@users.noreply.github.com>
2025-01-13 12:00:22 +0100

    feat: update dependencies to include preact-router and react-router-dom with aliasing for compatibility

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,12 @@ import preact from '@preact/preset-vite';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [preact()],
+	resolve: {
+		alias: {
+		  react: 'preact/compat',
+		  'react-dom': 'preact/compat',
+		},
+	  },
 	server: {
 		port: 5173,
 		host: '0.0.0.0',
