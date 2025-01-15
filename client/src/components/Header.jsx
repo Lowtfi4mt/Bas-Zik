07662a4c312494f7db34275e500333fef56c65fe
estelle <estelle_mgd>
2025-01-15 14:48:30 +0100

    modif headers

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,19 +3,51 @@ import { useLocation } from 'preact-iso';
 export function Header() {
 	const { url } = useLocation();
 
+	const profile = JSON.parse(localStorage.getItem('profile'));
+
+	const buttonstyle = {
+		display: 'inline-block',
+		padding: '10px 20px',
+		border: `2px solid ${profile.layout.theme.primary}`,
+		borderRadius: '5px',
+		color: profile.layout.theme.primary,
+		backgroundColor: 'white',
+		textDecoration: 'none',
+		transition: 'all 0.3s ease',
+	}
+
 	return (
 		<header>
+			<div
+      		style={{
+        	display: 'flex',
+        	gap: '20px',
+			padding: '20px',
+			flexWrap: 'wrap',
+			backgroundColor: profile.layout.theme.background,
+			}}
+			>
 			<nav>
-				<a href="/" class={url == '/' && 'active'}>
-					Welcome
-				</a>
-				<a href="/home" class={url == '/404' && 'active'}>
-					home
-				</a>
-				<a href="/music" class={url == '/music' && 'active'}>
-					Music
+				<a href="/" class={url == '/' && 'active'} style={buttonstyle} >
+					💿 Welcome
 				</a>
 			</nav>
+			<nav>
+				<a href="/home" class={url == '/404' && 'active'} style={buttonstyle}>
+					🏠 Home
+				</a>
+			</nav>
+			<nav>
+				<a href="/music" class={url == '/music' && 'active'} style={buttonstyle}>
+					🎵 Music
+				</a>
+			</nav>
+			<nav>
+				<a href="/layout" class={url == '/layout' && 'active'} style={buttonstyle}>
+					🎧 MyLayout
+				</a>
+			</nav>
+			</div>
 		</header>
 	);
 }
