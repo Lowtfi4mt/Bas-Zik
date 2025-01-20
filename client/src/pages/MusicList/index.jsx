0af600b77fdcde3fa63eca5c537c3358854f11c2
Antoine CHEUCLE <antoine.cheucle@imt-atlantique.net>
2025-01-20 14:18:56 +0100

    feat(playlist): Add ability to reorder playlist

--- client/src/pages/MusicList/index.jsx
+++ client/src/pages/MusicList/index.jsx
@@ -1,16 +1,49 @@
 import './style.css';
 import { usePlaylist } from '../../contexts/PlaylistContext';
 import MusicCard from '../../components/MusicCard';
+import { useRef, useState } from 'preact/hooks';
+
 
 export const MusicList = () => {
-  const { playlist, currentTrackIndex } = usePlaylist()
+  const { playlist, currentTrackIndex, setPlaylist, setCurrentTrackIndex } = usePlaylist();
+  const [items, setItems] = useState(playlist);
+  const dragItem = useRef();
+  const dragOverItem = useRef();
+
+  const handleSort = () => {
+    let _items = [...items];
+    const draggedItemContent = _items.splice(dragItem.current, 1)[0];
+    _items.splice(dragOverItem.current, 0, draggedItemContent);
+
+    setItems(_items);
+    setPlaylist(_items);
+
+    if (dragItem.current === currentTrackIndex) {
+      setCurrentTrackIndex(dragOverItem.current);
+    // @ts-ignore
+    } else if (dragItem?.current < currentTrackIndex && dragOverItem?.current >= currentTrackIndex) {
+      setCurrentTrackIndex(currentTrackIndex - 1);
+    // @ts-ignore
+    } else if (dragItem?.current > currentTrackIndex && dragOverItem?.current <= currentTrackIndex) {
+      setCurrentTrackIndex(currentTrackIndex + 1);
+    }
+  };
 
   return (
     <div className="music-list">
       <h1>Liste de lecture</h1>
       <ul className="playlist">
-        {playlist.map((track, index) => (
-          <MusicCard key={index} music={track} nowPlaying={index - currentTrackIndex} />
+        {items.map((track, index) => (
+          <li
+            key={track.id}
+            draggable
+            onDragStart={() => (dragItem.current = index)}
+            onDragEnter={() => (dragOverItem.current = index)}
+            onDragEnd={handleSort}
+            onDragOver={(e) => e.preventDefault()}
+          >
+            <MusicCard music={track} nowPlaying={index - currentTrackIndex} />
+          </li>
         ))}
       </ul>
     </div>
