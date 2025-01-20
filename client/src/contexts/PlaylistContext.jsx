b7bc89faee35a510319dd7967bb44b0647fe2a43
Antoine CHEUCLE <antoine.cheucle@imt-atlantique.net>
2025-01-20 12:37:26 +0100

    feat(playlist): Save playlist into localStorage

--- client/src/contexts/PlaylistContext.jsx
+++ client/src/contexts/PlaylistContext.jsx
@@ -1,12 +1,26 @@
 import { createContext } from 'preact';
-import { useContext, useState } from 'preact/hooks';
+import { useContext, useState, useEffect } from 'preact/hooks';
 
 const PlaylistContext = createContext(null);
 
 export const PlaylistProvider = ({ children }) => {
 
-  const [playlist, setPlaylist] = useState([]);
-  const [currentTrackIndex, setCurrentTrackIndex] = useState(null);
+  const [playlist, setPlaylist] = useState(() => {
+    const playlistData = localStorage.getItem('playlist');
+    return playlistData ? JSON.parse(playlistData).playlist : [];
+  });
+  const [currentTrackIndex, setCurrentTrackIndex] = useState(() => {
+    const playlistData = localStorage.getItem('playlist');
+    return playlistData ? JSON.parse(playlistData).currentTrackIndex : null;
+  });
+
+  useEffect(() => {
+      if (playlist) {
+        localStorage.setItem('playlist', JSON.stringify({ playlist, currentTrackIndex }));
+      } else {
+        localStorage.removeItem('playlist');
+      }
+    }, [playlist, currentTrackIndex]);
 
   return (
     <PlaylistContext.Provider value={{ playlist, setPlaylist, currentTrackIndex, setCurrentTrackIndex }}>
