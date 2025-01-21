413c3d0c58c77cb2ad39ed826beb6e253d768e09
nathan-claeys <127297857+nathan-claeys@users.noreply.github.com>
2025-01-21 13:12:24 +0100

    style: enhance MusicCard component with dynamic theming and improved layout

--- client/src/components/MusicCard.jsx
+++ client/src/components/MusicCard.jsx
@@ -7,6 +7,7 @@ import { usePlaylist } from "../contexts/PlaylistContext";
 
 const MusicCard = ({ music, nowPlaying = null }) => {
     const [menuOpen, setMenuOpen] = useState(false);
+    const theme = JSON.parse(localStorage.getItem('profile')).layout.theme;
 
     const { playlist, setPlaylist, currentTrackIndex, setCurrentTrackIndex } = usePlaylist();
 
@@ -65,58 +66,60 @@ const MusicCard = ({ music, nowPlaying = null }) => {
     }, [menuOpen]);
 
     return (
-        <div className={`music-card ${nowPlaying == 0 ? "now-playing" : ""} ${nowPlaying < 0 ? "passed" : ""}`}>
-            {/* Image */}
-            <img src={image} alt={title} className="music-image" />
+        <div>
+            <div className={`music-card ${nowPlaying == 0 ? "now-playing" : ""} ${nowPlaying < 0 ? "passed" : ""}`} 
+            style={{backgroundColor: nowPlaying == 0 ? theme.primary : (nowPlaying < 0 ? "white" : theme.background)}}>
+                {/* Image */}
+                <img src={image} alt={title} className="music-image" />
 
-            {/* Infos principales */}
-            <div className="music-info">
-                {nowPlaying == 0 && <span>▶ En cours de lecture...</span>}
-                <h3 className="music-title">{title}</h3>
-                <p className="music-meta">
-                    {music.authors.length > 0 ? (
-                        music.authors.map((author, index) => (
-                            <span key={index}>
-                                <Link to={`/app/artist/${music.authorsId[index]}`}>
-                                    {author}
-                                </Link>
-                                {index < music.authors.length - 1 && " et "}
-                            </span>
-                        ))
-                    ) : (
-                        <span>Artiste inconnu</span>
-                    )}
-                      &bull; {
-                        music.albums.length > 0 ? (
-                            music.albums.map((album, index) => (
+                {/* Infos principales */}
+                <div className="music-info">
+                    {nowPlaying == 0 && <span class="span-lecture">▶ En cours de lecture...</span>}
+                    <h3 className="music-title">{title}</h3>
+                    <p className="music-meta">
+                        {music.authors.length > 0 ? (
+                            music.authors.map((author, index) => (
                                 <span key={index}>
-                                    <Link to={`/app/album/${music.albumsId[index]}`}>
-                                        {album}
+                                    <Link to={`/app/artist/${music.authorsId[index]}`}>
+                                        <a class="music-author">{author}</a>
                                     </Link>
-                                    {index < music.albums.length - 1 && " et "}
+                                    {index < music.authors.length - 1 && " et "}
                                 </span>
                             ))
                         ) : (
-                            <span>Album inconnu</span>
-                        )
-                      }
-                </p>
-                <p className="music-stats">
-                    <span>{duration}</span> &bull; <span>{likes} likes</span>
-                </p>
-            </div>
+                            <span>Artiste inconnu</span>
+                        )}
+                        &bull; {
+                            music.albums.length > 0 ? (
+                                music.albums.map((album, index) => (
+                                    <span key={index}>
+                                        <Link to={`/app/album/${music.albumsId[index]}`}>
+                                            {album}
+                                        </Link>
+                                        {index < music.albums.length - 1 && " et "}
+                                    </span>
+                                ))
+                            ) : (
+                                <span>Album inconnu</span>
+                            )
+                        }
+                    </p>
+                    <p className="music-stats">
+                        <span>{duration}</span> &bull; <span>{likes} likes</span>
+                    </p>
+                </div>
 
-            {/* Boutons d'action */}
-            <div className="music-actions">
-                <button className="play-button" onClick={handlePlayNow}>▶</button>
-                <button
-                    className="more-options-button"
-                    onClick={() => setMenuOpen(!menuOpen)}
-                >
-                    ⋮
-                </button>
+                {/* Boutons d'action */}
+                <div className="music-actions">
+                    <button className="play-button" onClick={handlePlayNow} style={{backgroundColor: theme.secondary}}>▶</button>
+                    <button
+                        className="more-options-button"
+                        onClick={() => {setMenuOpen(!menuOpen);console.log(menuOpen)}}
+                    >
+                        ⋮
+                    </button>
+                </div>
             </div>
-
             {/* Menu contextuel */}
             {menuOpen && (
                 <div className="context-menu">
@@ -125,16 +128,20 @@ const MusicCard = ({ music, nowPlaying = null }) => {
                         <li onClick={handlePlayNext}>Lire ensuite</li>
                         <li onClick={handleAddToQueue}>Ajouter à la file d&apos;attente</li>
                         <li>Ajouter à une liste de lecture</li>
-                        {music.albumsId.length > 0 && (
-                            <Link to={`/app/album/${music.albumsId[0]}`}>
-                                <li>Accéder à l&apos;album</li>
-                            </Link>
-                        )}
-                        {music.authorsId.length > 0 && (
-                            <Link to={`/app/artist/${music.authorsId[0]}`}>
-                                <li>Accéder à l&apos;artiste</li>
-                            </Link>
-                        )}
+                        <li>
+                            {music.albumsId.length > 0 && (
+                                <Link to={`/app/album/${music.albumsId[0]}`} class="context-menu-text">
+                                    Accéder à l&apos;album
+                                </Link>
+                            )}
+                        </li>
+                        <li>
+                            {music.authorsId.length > 0 && (
+                                <Link to={`/app/artist/${music.authorsId[0]}`} class="context-menu-text">
+                                    Accéder à l&apos;artiste
+                                </Link>
+                            )}
+                        </li>
                         <li>Ajouter un j&apos;aime</li>
                         {playlist.some(track => track.id === music.id) && (
                             <li onClick={handleRemoveFromPlaylist}>
