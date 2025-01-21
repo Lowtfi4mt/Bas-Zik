88300091ff2979edf64602e3e47925bcabdedc38
Phoenixcresset <jeanbaptiste.lambertin@gmail.com>
2025-01-21 17:11:53 +0100

    feat: Implements gunicorn to serve the flask app

--- server/src/app.py
+++ server/src/app.py
@@ -65,3 +65,7 @@ def create_app() -> Flask:
 if __name__ == "__main__":
     flask_app = create_app()
     flask_app.run(host="0.0.0.0", port=5000, debug=True)
+
+
+if __name__ != "__main__":
+    app = create_app()
