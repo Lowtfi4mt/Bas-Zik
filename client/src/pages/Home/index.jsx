a7232f63943ad03e92147ea9376503e3b828ca9b
estelle <estelle_mgd>
2025-01-21 09:56:11 +0100

    modifs home

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -12,11 +12,14 @@ export function Home () {
 			<div className="titre-section" style={{ color: profile.layout.theme.secondary }}>Top 10</div>
 			<div className="listenAll" style={{color: theme.primary}}><div className="listenTop"> ▶️  Ecouter tout</div></div>
 		</div></div>
-		<div className="composant"><h2 style={{ color: profile.layout.theme.secondary }}>Mes listes de lecture</h2></div>
 		<div className="composant"> <div className="titre">
-			<div className="titre-section" style={{ color: profile.layout.theme.secondary }}>Découvertes du moment</div>
-			<div className="listenAll" style={{color: theme.primary}}><div className="listenProp"> ▶️  Ecouter tout</div></div>
+			<div className="titre-section" style={{ color: profile.layout.theme.secondary }}>Mes listes de lecture</div>
+			<div className="listenAll" style={{color: theme.primary}}><div className="newList"> + Nouvelle liste</div></div>
 		</div></div>
+		<div className="composant"> <div className="titre">
+			<div className="titre-section" style={{ color: profile.layout.theme.secondary }}>Top Propositions</div></div>
+			<p style={{color: theme.secondary}} >N'oublie pas de voter pour les prochaines musiques sur la plateforme !!</p>
+		</div>
 	  </div>
 	  {/*<div>
 		<h2 style={{ color: profile.layout.theme.secondary }}>User Information</h2>
